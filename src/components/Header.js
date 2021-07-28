571f29dc24d0a190c006ab23f0b10b6f1f890a2e
tvt97 <thanhtrinh.dev@gmail.com>
2021-07-28 10:02:55 +0700

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,6 +18,19 @@ export function Header() {
                 <div className="nav-item">Sign in</div>
                 <div className="singup-btn"><button onClick={()=>dispatch(setOpenPopup(true))}>Sign up</button></div>
             </div>
+
+            <div id="toggle-button" onClick={() => setOpenMenu(true)}><i className="fa fa-bars" aria-hidden="true"></i></div>
+
+            {openmenu === true && <div id="mobile-menu">
+                <button className="mobile_close" onClick={() => setOpenMenu(false)}>x</button>
+                <div className="mobile-menu-content">
+                    <div className="nav-item"><Link to='/flight-search'>Flights</Link></div>
+                    <div className="nav-item">Hotels</div>
+                    <div className="nav-item">Packages</div>
+                    <div className="nav-item">Sign in</div>
+                    <div className="nav-item" onClick={()=>dispatch(setOpenPopup(true))}>Sign up</div>
+                </div>
+            </div>}
         </div>
     )
 }
